4b4096b17066a36229630bf3bfba406081a5e672
Brown Chou <oo674595@gmail.com>
2020-08-19 15:03:57 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,10 @@
+country = input('請輸入您的國家: ')
+age = input('請輸入您的年齡: ')
+age = int(age)
+if country == 'Taiwan':
+	if age >= 18:
+		print('您可以考取駕照')
+	else:
+		print('您還無法考取駕照')
+else:
+	print('您並非台灣人')
